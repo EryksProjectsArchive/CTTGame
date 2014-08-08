4df3758134f1d7caf173ba5b55bdeb9d9d572ec1
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2014-08-09 01:52:30 +0200

    Added SharedPtr

---
+++ src/game/ctt/core/SharedPtr.h
@@ -0,0 +1,63 @@
+//////////////////////////////////////////////
+//
+//		   City Transport Tycoon
+//	   Copyright (C) Black Ice Mountains
+//		 	All rights reserved
+//
+// File		: core/SharedPtr.h
+// Author	: Eryk Dwornicki
+//
+//////////////////////////////////////////////
+
+#pragma once
+
+template <class Type>
+class SharedPtr
+{
+private:
+	Type * m_value;
+	unsigned int * m_refCount;
+
+public:
+	SharedPtr()
+		: m_refCount(new unsigned int), m_value(0)
+	{
+		*m_refCount = 1;
+	}
+
+	SharedPtr(Type * value)
+		: m_refCount(new unsigned int), m_value(value)
+	{
+		*m_refCount = 1;
+	}
+
+	SharedPtr(const SharedPtr& sharedPtr)
+		: m_refCount(sharedPtr.m_refCount), m_value(sharedPtr.m_value)
+	{
+		++ *m_refCount;
+	}
+
+	~SharedPtr()
+	{
+		printf("~SharedPtr\n");
+		if (--*m_refCount <= 0)
+		{
+			delete m_value;	
+			delete m_refCount;
+			printf("REMOVE OBJECT\n");
+		}
+	}
+
+	operator Type& () { return m_value; }
+	operator Type*() { return m_value; }
+	Type * operator*() { return m_value; }
+	Type& operator&() { return m_value; }
+
+	SharedPtr<Type>& operator=(const SharedPtr& sharedPtr)
+	{
+		m_refCount = sharedPtr.m_refCount;
+		m_value = sharedPtr.m_value;
+		*m_refCount++;
+		return *this;
+	}
+};
