0379c4ab63d7d0e1820c0ea3b6cd4d19cca4079e
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2014-09-29 00:07:45 +0200

    - Removed day night cycle from environment (redo it and make it better) - Removed simple implementation of FT

--- src/game/ctt/game/environment/Environment.cpp
+++ src/game/ctt/game/environment/Environment.cpp
@@ -13,22 +13,17 @@
 
 #include <core/Logger.h>
 #include <os/OS.h>
+#include <core/Timer.h>
 
-Environment::Environment() : m_time(0)
+Environment::Environment() : m_time(5*60)
 {
-	m_states[EnvironmentStates::Day] = EnvironmentState(Vector3(30.0f, 10.0f, 0.0f), glm::vec3(1, 1, 1), glm::vec3(0.96f, 0.92f, 0.76f), 100.0f, 5, 6);
-	m_states[EnvironmentStates::Night] = EnvironmentState(Vector3(30.0f, 10.0f, 0.0f), glm::vec3(1, 1, 1), glm::vec3(0.03f, 0.09f, 0.25f), 50.0f, 20, 22);
+	m_sunAmbientColor = Vector3(0.8f, 0.8f, 0.8f);
+	m_sunLightColor = Vector3(1.0f, 207/255, 114/255);
+	m_sunLightPower = 10.0f;
+	m_sunPosition = Vector3(0.0f, 10.0f, 0.0f);
 
-	m_currentState = EnvironmentStates::Day;
-	m_nextState = EnvironmentStates::Night;
-
-	m_sunAmbientColor = m_states[m_currentState].sunAmbientColor;
-	m_sunLightColor = m_states[m_currentState].sunLightColor;
-	m_sunLightPower = m_states[m_currentState].sunLightPower;
-	m_sunPosition = m_states[m_currentState].sunPosition;
-
-	m_timeScale = 1000; // 1 minute per 1 second (To be fixed soon)
-	m_lastTimeUpdate = OS::getMicrosecondsCount() / 1000;
+	m_timeScale = 1000;
+	m_deltaUpdate = 0;
 }
 
 Environment::~Environment()
@@ -36,7 +31,7 @@ Environment::~Environment()
 
 }
 
-void Environment::setSunPosition(glm::vec3 position)
+void Environment::setSunPosition(Vector3 position)
 {
 	m_sunPosition = position;
 }
@@ -48,52 +43,44 @@ glm::vec3 Environment::getSunPosition()
 
 bool Environment::isNight()
 {
-	return m_currentState == 1;
+	return m_time <= 5 * 60 && m_time >= 20 * 60;
 }
 
 void Environment::setTime(unsigned char hour, unsigned char minute)
 {
-	m_time = (hour << 8) | minute;
+	m_time = hour * 60 + minute;
 }
 
 unsigned char Environment::getHour()
 {
-	return (m_time >> 8) & 0xFF;
+	return m_time / 60 % 24;
 }
 
 unsigned char Environment::getMinute()
 {
-	return m_time & 0xFF;
+	return m_time % 60;
 }
 
 void Environment::pulse()
 {
-	unsigned char minute = m_time & 0xFF;
-	unsigned char hour = (m_time >> 8) & 0xFF;
+	int timeElapsed = 0;
 
-	unsigned long long now = OS::getMicrosecondsCount() / 1000;
-	unsigned char minElapsed = ((now - m_lastTimeUpdate) / m_timeScale) &0xFF;
-	
-	if (minElapsed > 0)
+	m_deltaUpdate += Timer::getDeltaTime();
+	while(m_deltaUpdate >= m_timeScale / 100)
 	{
-		minute += minElapsed;
-		if (minute > 59)
-		{
-			minute = 0;
-			if (++hour >= 24)
-			{
-				hour = 0;
-			}
-		}
-		m_lastTimeUpdate = now;
+		timeElapsed++;
+		m_deltaUpdate -= m_timeScale / 100;
 	}
 
-	m_time = (hour << 8) | minute;
-
-	interpolateStates();
+	
+	if (timeElapsed > 0)
+	{
+		m_time += timeElapsed;
+		printf("TIME %02d:%02d\n", getHour(), getMinute());
+	}
 }
 
-glm::vec3 Environment::getSunLightColor()
+Vector3 Environment::getSunLightColor()
 {
 	return m_sunLightColor;
 }
@@ -103,63 +90,7 @@ float Environment::getSunLightPower()
 	return m_sunLightPower;
 }
 
-glm::vec3 Environment::getSunAmbientColor()
+Vector3 Environment::getSunAmbientColor()
 {
 	return m_sunAmbientColor;
-}
-
-void Environment::interpolateStates()
-{
-	if (true) // disable interpolateStates - we dont need it for now
-		return;
-
-	bool interpStart = getHour() == m_states[m_nextState].interpStart;
-	bool start = interpStart && !m_stateInterpolation.interpolating;
-
-	if (m_stateInterpolation.interpolating || start)
-	{
-		unsigned char hour = getHour();
-		unsigned char minutes = getMinute();
-		if (start)
-		{
-			m_stateInterpolation.start = hour * 60 + minutes;
-			m_stateInterpolation.end = m_stateInterpolation.start + m_states[m_nextState].interpEnd * 60;
-			m_stateInterpolation.interpolating = true;
-		}
-		m_stateInterpolation.current = hour * 60 + minutes;
-
-		float alpha = (float)(m_stateInterpolation.current - m_stateInterpolation.start) / (m_stateInterpolation.end - m_stateInterpolation.start);
-
-		bool interpEnd = false;
-		if (alpha >= 1.0f)
-		{
-			alpha = 1.0f;
-
-			interpEnd = true;
-		}
-
-		glm::vec3 start = m_states[m_currentState].sunLightColor;
-		glm::vec3 end = m_states[m_nextState].sunLightColor;
-		m_sunLightColor = start + (end - start) * alpha;
-
-		start = m_states[m_currentState].sunAmbientColor;
-		end = m_states[m_nextState].sunAmbientColor;
-		m_sunAmbientColor = start + (end - start) * alpha;
-
-		float _start = m_states[m_currentState].sunLightPower;
-		float _end = m_states[m_nextState].sunLightPower;
-		m_sunLightPower = _start + (_end - _start) * alpha;
-
-		start = m_states[m_currentState].sunPosition;
-		end = m_states[m_nextState].sunPosition;
-		m_sunPosition = start + (end - start) * alpha;
-
-		if (interpEnd)
-		{
-			EnvironmentStates::Type tmp = m_currentState;
-			m_currentState = m_nextState;
-			m_nextState = tmp;
-			m_stateInterpolation.interpolating = false;
-		}
-	}
 }
--- src/game/ctt/game/environment/Environment.h
+++ src/game/ctt/game/environment/Environment.h
@@ -14,72 +14,24 @@
 #include <Prerequisites.h>
 #include <core/Singleton.h>
 
-struct EnvironmentStates
-{
-	enum Type
-	{
-		Day,
-		Night,
-		Count
-	};
-};
-
-struct EnvironmentState
-{
-	glm::vec3 sunPosition;
-	glm::vec3 sunLightColor;
-	glm::vec3 sunAmbientColor;
-	float sunLightPower;
-	unsigned char interpStart;
-	unsigned char interpEnd;
-
-	EnvironmentState()
-	{
-		sunLightPower = 0;
-	}
-
-	EnvironmentState(glm::vec3 position, glm::vec3 lightColor, glm::vec3 ambientColor, float lightPower, unsigned char _interpStart, unsigned char _interpEnd)
-	{
-		sunPosition = position;
-		sunLightColor = lightColor;
-		sunAmbientColor = ambientColor;
-		sunLightPower = lightPower;
-		interpStart = _interpStart;
-		interpEnd = _interpEnd;
-	}
-};
 
 class Environment : public Singleton < Environment >
 {
 private:
-	glm::vec3 m_sunPosition;
+	Vector3 m_sunPosition;
 	float m_sunLightPower;
-	glm::vec3 m_sunLightColor;
-	glm::vec3 m_sunAmbientColor;
+	Vector3 m_sunLightColor;
+	Vector3 m_sunAmbientColor;
 
 	unsigned int m_time;	
-
-	EnvironmentState m_states[EnvironmentStates::Count];
-	EnvironmentStates::Type m_currentState;
-	EnvironmentStates::Type m_nextState;
-	struct
-	{
-		unsigned char start;
-		unsigned char end;
-		unsigned char current;
-		bool interpolating;
-	} m_stateInterpolation;
-
 	unsigned int m_timeScale;
-	unsigned long long m_lastTimeUpdate;
-
-	void interpolateStates();
+	float m_deltaUpdate;
 public:
 	Environment();
 	~Environment();
 
-	void setSunPosition(glm::vec3 position);
-	glm::vec3 getSunPosition();
+	void setSunPosition(const Vector3 position);
+	Vector3 getSunPosition();
 
 	bool isNight();
 
@@ -89,7 +41,7 @@ public:
 
 	void pulse();
 
-	glm::vec3 getSunLightColor();
+	Vector3 getSunLightColor();
 	float getSunLightPower();
-	glm::vec3 getSunAmbientColor();
+	Vector3 getSunAmbientColor();
 };
