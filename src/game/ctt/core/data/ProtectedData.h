084cf4d2c63de75a60cec3965fb4d6ae29b95736
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2015-01-21 00:24:36 +0100

    Added ProtectedData class.

---
+++ src/game/ctt/core/data/ProtectedData.h
@@ -0,0 +1,72 @@
+//////////////////////////////////////////////
+//
+//		   City Transport Tycoon
+//	   Copyright (C) Black Ice Mountains
+//		 	All rights reserved
+//
+// File		: core/data/ProtectedData.h
+// Author	: Eryk Dwornicki
+//
+//////////////////////////////////////////////
+
+#pragma once
+
+#include <Prerequisites.h>
+
+static uint8 g_protectMagic = (uint8)'G';
+
+template <typename T>
+class ProtectedData
+{
+private:
+	T m_data;
+public:
+	ProtectedData()
+	{
+	}
+
+	void set(T data)
+	{
+		uint8 *inBuffer = (uint8 *)&data;
+		uint8 *buffer = (uint8 *)&m_data;
+		for (uint32 i = 0; i < sizeof(T); ++i)
+		{
+			uint8 inByte = *(uint8 *)(inBuffer + i);
+			*(uint8 *)(buffer + i) = ~(inByte ^ g_protectMagic);
+		}
+	}
+
+	T get()
+	{
+		uint8 outBuffer[sizeof(T)] = { 0 };
+		uint8 * buffer = (uint8 *)&m_data;
+		for (uint32 i = 0; i < sizeof(T); ++i)
+		{
+			uint8 byte = *(uint8 *)(buffer + i);
+			outBuffer[i] = ~(byte ^ g_protectMagic);
+		}
+
+		return *(T *)outBuffer;
+	}
+
+	inline T val() 
+	{
+		return get() 
+	};
+
+	inline T value() 
+	{
+		return get();
+	};
+
+	ProtectedData& operator=(T value)
+	{
+		set(value);
+		return *this;
+	}
+
+	operator T()
+	{
+		return get();
+	}
+};
